2d7fd76bbc2a177fc6b0c996e426027d5e16eb07
amalsacko999 <amalsacko999@gmail.com>
2026-09-01 20:24:26 +0000

    Update chatbot.js

--- commands/chatbot.js
+++ commands/chatbot.js
@@ -221,7 +221,7 @@ async function handleChatbotResponse(sock, chatId, message, userMessage, senderI
     }
 }
 
-const GROQ_API_KEY = process.env.GROQ_API_KEY || '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';
+const GROQ_API_KEY = process.env.GROQ_API_KEY || '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';
 
 async function getAIResponse(userMessage, userContext) {
     try {
